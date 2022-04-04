0981a99462a2f096cc18a540cce52efba00a482c
d-rocham <d_rocham@outlook.com>
2022-04-04 16:31:59 -0500

    Added NOT NULL constraint on Level.level_prize model

--- backend/app/models.py
+++ backend/app/models.py
@@ -6,7 +6,7 @@ class Level(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, unique=True)
     level_name = db.Column(db.String(10), nullable=False, unique=True)
-    level_prize = db.Column(db.Integer)
+    level_prize = db.Column(db.Integer, nullable=False)
 
 
 class Questions(db.Model):
