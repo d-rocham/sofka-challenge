2a1f8acc0ed60fb6ac09311c009581733e5ba94a
d-rocham <d_rocham@outlook.com>
2022-04-04 15:26:08 -0500

    Minor changes in model declarations, applied migrations. Created 'create_question'  classmethod in model Questions

--- backend/app/models.py
+++ backend/app/models.py
@@ -17,11 +17,37 @@ class Questions(db.Model):
     level_id = db.Column(db.ForeignKey("levels.id"), index=True, nullable=False)
     question_text = db.Column(db.String(140), nullable=False)
 
+    @classmethod
+    def create_question(cls, question, answers):
+        """Inserts a question inside Questions table and its associated answers inside Answers table
+        Args:
+            question: `dictionary` where the key is a `string` question_text
+            and the value is an `int` representing the level_id
+
+            answers: `dictionary`with 4 key, value pairs where each key is a `string` representing
+            `Answers.answer_text` and the value is an `int` representing Answers.correct`
+
+        Returns: TODO:
+        """
+        new_question = cls(
+            level_id=question["level_id"], question_text=question["question_text"]
+        )
+
+        db.session.add(new_question)
+        db.session.commit()
+
+        for key, value in answers.items():
+            new_answer = Answers(
+                question_id=new_question.id, answer_text=key, correct=value
+            )
+            db.session.add(new_answer)
+            db.session.commit()
+
 
 class Answers(db.Model):
     __tablename__ = "answers"
 
     id = db.Column(db.Integer, primary_key=True, unique=True)
-    level_id = db.Column(db.ForeignKey("questions.id"), index=True, nullable=False)
-    answer_text = db.Column(db.String(15), nullable=False)
+    question_id = db.Column(db.ForeignKey("questions.id"), index=True, nullable=False)
+    answer_text = db.Column(db.String(25), nullable=False)
     correct = db.Column(db.Integer, nullable=False)
